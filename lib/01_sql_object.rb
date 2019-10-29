2a060f8c00ddc721057b2081a65f5b4629f0863d
your name here <your_email_here>
2019-10-29 09:18:19 -0400

    Complete part 1

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -81,18 +81,38 @@ class SQLObject
   end
 
   def attribute_values
-    # ...
+    self.class.columns.map { |attr| self.send(attr) }
   end
 
   def insert
-    # ...
+    cols = self.class.columns.drop(1)
+    col_names = cols.map(&:to_s).join(", ")
+    question_marks = (["?"] * cols.count).join(", ")
+
+    DBConnection.execute(<<-SQL, *attribute_values.drop(1))
+      INSERT INTO
+        #{self.class.table_name} (#{col_names})
+      VALUES
+        (#{question_marks})
+    SQL
+
+    self.id = DBConnection.last_insert_row_id
   end
 
   def update
-    # ...
+    set_line = self.class.columns.map { |attr| "#{attr} = ?" }.join(", ")
+
+    DBConnection.execute(<<-SQL, *attribute_values, id)
+      UPDATE
+        #{self.class.table_name}
+      SET
+        #{set_line}
+      WHERE
+        #{self.class.table_name}.id = ?
+    SQL
   end
 
   def save
-    # ...
+    id.nil? ? insert : update
   end
 end
