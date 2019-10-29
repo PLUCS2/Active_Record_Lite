25d94632b1fdc6e36c640f03ae3babd82ca1a4c5
your name here <your_email_here>
2019-10-29 10:00:08 -0400

    Part 3

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,10 +3,22 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+
+    wheres = params.keys.map { |key| "#{key} = ?" }.join(" AND ")
+
+    results = DBConnection.execute(<<-SQL, *params.values)
+      SELECT
+        *
+      FROM
+        #{table_name}
+      WHERE
+        #{wheres}
+    SQL
+
+    parse_all(results)
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -10,23 +10,39 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    @class_name.constantize 
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    defaults = {
+      :foreign_key => "#{name}_id".to_sym,
+      :class_name => name.to_s.camelcase,
+      :primary_key => :id
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end 
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    defaults = {
+      :foreign_key => "#{self_class_name.underscore}_id".to_sym,
+      :class_name => name.to_s.singularize.camelcase,
+      :primary_key => :id
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
   end
 end
 
